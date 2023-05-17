3a286194f444f0f3260cdf4259b97b1ee4db5789
Gabriel <gabrielfernandesthiago@hotmail.com>
2023-05-16 22:16:54 -0300

    add funcao plotar grafico

--- dijkstra-centrality.py
+++ dijkstra-centrality.py
@@ -1,5 +1,7 @@
 import sys
 from queue import PriorityQueue
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class Grafo:
     def __init__(self, numVertices):
@@ -52,6 +54,27 @@ class Grafo:
                     print(f"{self.matAdj[i][j]:>3}", end=" ")
             print("")
 
+    def plotarGrafo(self):
+        G = nx.Graph()
+        numVertices = len(grafo.matAdj)
+        numvertices = grafo.numVertices
+        G.add_nodes_from(range(numvertices))
+
+        for i in range(numvertices):
+            for j in range(i + 1, numvertices):
+                weight = grafo.matAdj[i][j]
+                if weight != sys.maxsize:
+                    G.add_edge(i, j, weight=weight)
+
+        pos = nx.spring_layout(G)  # Layout para posicionar os nós
+        edge_labels = nx.get_edge_attributes(G, 'weight')  # Rótulos das arestas
+        nx.draw_networkx(G, pos)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        plt.axis('off')
+        plt.show()
+
+
+
 grafo = Grafo(5)
 grafo.adicionarAresta(0, 1, 4)
 grafo.adicionarAresta(0, 2, 1)
@@ -63,6 +86,9 @@ grafo.adicionarAresta(3, 4, 3)
 caminhoMinimo = grafo.dijkstra(0)  # partindo do vértice 0, encontre o caminho mínimo
 
 grafo.mostraGrafo()
+
+#Plotando o grafo
+grafo.plotarGrafo()
 
 print(f"\nCaminho Mínimo partindo do vértice 0 e len(caminhoMinimo) = {len(caminhoMinimo)}: ")
 for i in range(len(caminhoMinimo)):
